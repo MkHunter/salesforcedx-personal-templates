2c4c7eb819aec48feeb80dd01b2dede55b6dfbda
MkHunter <miguelernesto_23@hotmail.com>
2022-02-22 16:58:53 -0700

    Fix Apex Class & LWC Default

--- templates/lightningcomponent/lwc/default/default.js
+++ templates/lightningcomponent/lwc/default/default.js
@@ -1,3 +1,11 @@
+/*
+*   Author: MkHunter
+*   Description:- <%= className %> LWC
+*   Created:- //
+*   Last Updated:- //
+*/
 import { LightningElement } from 'lwc';
 
-export default class <%= className %> extends LightningElement {}
+export default class <%= className %> extends LightningElement {
+    
+}
